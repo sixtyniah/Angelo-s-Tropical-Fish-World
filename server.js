b36a8e9cc5e36583ceaf9bd967b6d41b13b37ef1
Niah O'Briant <niah@ad.unc.edu>
2024-01-16 00:24:46 -0500

    plz work

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use(compression());
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const SQUARE_SANDBOX_ACCESS_TOKEN = process.env.SQUARE_SANDBOX_ACCESS_TOKEN;
